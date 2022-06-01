ef43f2a315562131da7bc67f861e785cea0c4063
xz <2225915455@qq.com>
2022-06-01 18:56:45 +0800

    gai wei grass

--- z.py
+++ z.py
@@ -1,7 +1,11 @@
 
-c = int(input("请输入你的年龄："))
-
-if c >= 18:
-    print('恭喜你成年了！')
+grass = float(input("你的成绩："))
+if grass == 100:
+    print("哇哦，你太棒了！")
+elif 80 <= grass <100:
+    print("你已经很优秀了，继续加油！")
+elif 60 <= grass <80:
+    print("再接再厉哦！")
 else:
-    print("真可惜你还未成年！")
+    print("要加油哦！！！")
+
